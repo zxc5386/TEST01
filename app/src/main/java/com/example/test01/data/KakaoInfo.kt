be18c8d4ed017c4a13ae3f75701ad1bff03c6b1b
zxc5386 <0163765386@naver.com>
2022-03-29 13:33:57 +0900

    7777

--- app/src/main/java/com/example/test01/data/KakaoInfo.kt
+++ app/src/main/java/com/example/test01/data/KakaoInfo.kt
@@ -1,6 +1,11 @@
 package com.example.test01.data
 
+import com.google.gson.annotations.SerializedName
+
 data class KakaoInfo(
+    @SerializedName("secure_resource")
     val secure_resource: Boolean,
+
+    @SerializedName("property_keys")
     val property_keys: String
 )
--- app/src/main/java/com/example/test01/data/KakaoInfoService.kt
+++ app/src/main/java/com/example/test01/data/KakaoInfoService.kt
@@ -8,7 +8,7 @@ import retrofit2.http.POST
 interface KakaoInfoService {
 
     @FormUrlEncoded
-    @POST("/app_login/")
+    @POST("")
     fun requestKakaoLogin(
         @Field("Authorization") Authorization:String
     ) : Call<KakaoInfo>
